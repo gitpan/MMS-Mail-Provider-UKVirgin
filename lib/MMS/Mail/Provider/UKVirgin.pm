468ef0dcf1b4946038f92100781fe2b76b93a082
Rob Lee <robl@cpan.org>
2006-01-11 00:16:44 +0000

    Import of ROBL/MMS-Mail-Provider-UKVirgin-0.02 from CPAN.
    gitpan-cpan-distribution: MMS-Mail-Provider-UKVirgin
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ROBL/MMS-Mail-Provider-UKVirgin-0.02.tar.gz
    gitpan-cpan-author:       ROBL
    gitpan-cpan-maturity:     released

--- lib/MMS/Mail/Provider/UKVirgin.pm
+++ lib/MMS/Mail/Provider/UKVirgin.pm
@@ -13,11 +13,11 @@ MMS::Mail::Provider::UKVirgin - This provides a class for parsing an MMS::Mail::
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
